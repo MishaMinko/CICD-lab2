be550238a3751f48e5590d70976a70039d92ec2b
Minko <mf0981115811@gmail.com>
2024-04-01 19:34:25 +0300

    Drawing ship and guns

---
+++ classes/ship.py
@@ -0,0 +1,57 @@
+import pygame
+
+class Ship:
+    def __init__(self, name, img, pos, size, numGuns=0, gunPath=None, gunSize=None, gunCoords=None):
+        self.name = name
+        #vertical image
+        self.vImage = loadImage(img, size)
+        self.vImageWidth = self.vImage.get_width()
+        self.vImageHeight = self.vImage.get_height()
+        self.vImageRect = self.vImage.get_rect()
+        self.vImageRect.topleft = pos
+        #horizontal image
+        self.hImage = pygame.transform.rotate(self.vImage, -90)
+        self.hImageWidth = self.hImage.get_width()
+        self.hImageHeight = self.hImage.get_height()
+        self.hImageRect = self.hImage.get_rect()
+        self.hImageRect.topleft = pos
+        #image
+        self.image = self.vImage
+        self.rect = self.vImageRect
+        self.rotation = False
+        #guns
+        self.gunList = []
+        if numGuns > 0:
+            self.gunCoords = gunCoords
+            for i in range(numGuns):
+                self.gunList.append(Guns(gunPath, self.rect.center, (size[0] * gunSize[0], size[1] * gunSize[1]), self.gunCoords[i]))
+
+    
+    def draw(self, window):
+        window.blit(self.image, self.rect)
+        for guns in self.gunList:
+            guns.draw(window, self)
+
+
+class Guns:
+    def __init__(self, imgPath, pos, size, offset):
+        self.image = loadImage(imgPath, size, True)
+        self.offset = offset
+        self.rect = self.image.get_rect(center=pos)
+
+
+    def update(self, ship):
+        self.rect.center = (ship.rect.centerx, ship.rect.centery + (ship.image.get_height()//2 * self.offset))
+
+
+    def draw(self, window, ship):
+        self.update(ship)
+        window.blit(self.image, self.rect)
+
+
+def loadImage(path, size, rotate=False):
+    img = pygame.image.load(path).convert_alpha()
+    img = pygame.transform.scale(img, size)
+    if rotate == True:
+        img = pygame.transform.rotate(img, -90)
+    return img
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Програма написана Фурсенком Михайлом та Полярушом Данилом групи ІПЗ-22
-import pygame
-import random
+from classes.ship import Ship
+import pygame, random, os
 pygame.init()
 
 
@@ -47,10 +47,28 @@ def printGameLogic():
         print(_)
 
 
+def loadImage(path, size, rotate=False):
+    img = pygame.image.load(path).convert_alpha()
+    img = pygame.transform.scale(img, size)
+    if rotate == True:
+        img = pygame.transform.rotate(img, -90)
+    return img
+
+
+def createFleet():
+    fleet = []
+    for name in FLEET.keys():
+        fleet.append(Ship(name, FLEET[name][1], FLEET[name][2], FLEET[name][3], FLEET[name][4], FLEET[name][5], FLEET[name][6], FLEET[name][7]))
+    return fleet
+
+
 def updateGameScreen(window):
     window.fill((0, 0, 0))
 
     showGridOnScreen(window, CELLSIZE, pGameGrid, cGameGrid)
+
+    for ship in pFleet:
+        ship.draw(window)
 
     pygame.display.update()
 
@@ -67,13 +85,33 @@ CELLSIZE = 50
 GAMESCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption('BattleShip')
 
+current_directory = os.path.dirname(__file__)
+FLEET = {
+    'battleship': ['battleship', os.path.join(current_directory, 'assets', 'images', 'ships', 'battleship', 'battleship.png'), (125, 600), (40, 195),
+                   4, os.path.join(current_directory, 'assets', 'images', 'ships', 'battleship', 'battleshipgun.png'), (0.4, 0.125), [-0.525, -0.34, 0.67, 0.49]],
+    'cruiser': ['cruiser', os.path.join(current_directory, 'assets', 'images', 'ships', 'cruiser', 'cruiser.png'), (200, 600), (40, 195),
+                2, os.path.join(current_directory, 'assets', 'images', 'ships', 'cruiser', 'cruisergun.png'), (0.4, 0.125), [-0.36, 0.64]],
+    'destroyer': ['destroyer', os.path.join(current_directory, 'assets', 'images', 'ships', 'destroyer', 'destroyer.png'), (275, 600), (30, 145),
+                  2, os.path.join(current_directory, 'assets', 'images', 'ships', 'destroyer', 'destroyergun.png'), (0.5, 0.15), [-0.52, 0.71]],
+    'patrol boat': ['patrol boat', os.path.join(current_directory, 'assets', 'images', 'ships', 'patrol boat', 'patrol boat.png'), (425, 600), (20, 95),
+                    0, '', None, None],
+    'submarine': ['submarine', os.path.join(current_directory, 'assets', 'images', 'ships', 'submarine', 'submarine.png'), (350, 600), (30, 145),
+                  1, os.path.join(current_directory, 'assets', 'images', 'ships', 'submarine', 'submarinegun.png'), (0.25, 0.125), [-0.45]],
+    'carrier': ['carrier', os.path.join(current_directory, 'assets', 'images', 'ships', 'carrier', 'carrier.png'), (50, 600), (45, 245),
+                0, '', None, None],
+    'rescue ship': ['rescue ship', os.path.join(current_directory, 'assets', 'images', 'ships', 'rescue ship', 'rescue ship.png'), (500, 600), (20, 95),
+                    0, '', None, None]
+}
+
 
 #creating game variables
 pGameGrid = createGameGrid(ROWS, COLS, CELLSIZE, (50, 50))
 pGameLogic = createGameLogic(ROWS, COLS)
+pFleet = createFleet()
 
 cGameGrid = createGameGrid(ROWS, COLS, CELLSIZE, (SCREENWIDTH - (ROWS * CELLSIZE) - 50, 50))
 cGameLogic = createGameLogic(ROWS, COLS)
+cFleet = createFleet()
 
 printGameLogic()
 
