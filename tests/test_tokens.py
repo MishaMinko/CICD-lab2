596fe5af82798deaaff63cc276ec3dfc20d8a42b
Minko <mf0981115811@gmail.com>
2024-05-29 22:57:02 +0300

    test tokens draw

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -14,3 +14,10 @@ def token_data():
     pos = (5, 5)
     action = 'Hit'
     return image, pos, action
+
+def test_token_draw(pygame_init, token_data):
+    image, pos, action = token_data
+    token = Token(image, pos, action)
+    mock_window = pygame.Surface((100, 100))
+    token.draw(mock_window)
+    assert mock_window.get_at(pos) == (0, 0, 0, 255), "Expected token to be drawn at the correct position"
