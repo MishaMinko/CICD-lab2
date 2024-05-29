8684ada66e187558dc42c973667b9a7821ff27f9
Minko <mf0981115811@gmail.com>
2024-05-29 22:13:26 +0300

    test player make attack hit

--- tests/test_player.py
+++ tests/test_player.py
@@ -20,3 +20,13 @@ def globals():
         'cGameGrid': [[(x, y) for x in range(10)] for y in range(10)],
         'CELLSIZE': 50
     }
+
+def test_makeAttack_hit(pygame_init, globals):
+    player = Player(globals)
+    logicgrid = [[' ' for _ in range(10)] for _ in range(10)]
+    posX, posY = globals['pGameGrid'][0][0]
+    pygame.mouse.get_pos = lambda: (posX + 25, posY + 25)
+    player.makeAttack(globals['pGameGrid'], logicgrid)
+    assert logicgrid[0][0] == 'X', "Expected 'X' in logicgrid[0][0] after making a hit"
+    assert not player.turn, "Player turn should be set to False after making a hit"
+    assert any(token.action == 'Hit' for token in globals['TOKENS']), "A 'Hit' token should be added"
