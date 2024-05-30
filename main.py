b8de3e0f966887b36356af296ba0c0f2cae485a8
Minko <mf0981115811@gmail.com>
2024-05-30 09:53:24 +0300

    test main fixtures and fix

--- main.py
+++ main.py
@@ -205,137 +205,141 @@ def updateGameScreen(window):
 
     pygame.display.update()
 
+def main():
+    global SCREENWIDTH, SCREENHEIGHT, ROWS, COLS, CELLSIZE, DEPLOYMENT, TURNTIMER, GAMESTATE, GAMESCREEN
+    global FLEET, STAGE, MAINMENUIMAGE, ENDSCREENIMAGE, BUTTONS, difficulty_buttons, REDTOKEN, GREENTOKEN, BLUETOKEN, TOKENS
+    global pGameGrid, pGameLogic, pFleet, cGameGrid, cGameLogic, cFleet, player1, computer, winner
 
-#game settings
-SCREENWIDTH = 1260
-SCREENHEIGHT = 960
-ROWS = 10
-COLS = 10
-CELLSIZE = 50
-DEPLOYMENT = True
-TURNTIMER = pygame.time.get_ticks()
-GAMESTATE = 'Main Menu'
+    #game settings
+    SCREENWIDTH = 1260
+    SCREENHEIGHT = 960
+    ROWS = 10
+    COLS = 10
+    CELLSIZE = 50
+    DEPLOYMENT = True
+    TURNTIMER = pygame.time.get_ticks()
+    GAMESTATE = 'Main Menu'
 
+    #pygame display
+    GAMESCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+    pygame.display.set_caption('BattleShip')
 
-#pygame display
-GAMESCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-pygame.display.set_caption('BattleShip')
-
-current_directory = os.path.dirname(__file__)
-FLEET = {
-    'battleship': ['battleship', os.path.join(current_directory, 'assets', 'images', 'ships', 'battleship', 'battleship.png'), (125, 600), (40, 195),
-                   4, os.path.join(current_directory, 'assets', 'images', 'ships', 'battleship', 'battleshipgun.png'), (0.4, 0.125), [-0.525, -0.34, 0.67, 0.49]],
-    'cruiser': ['cruiser', os.path.join(current_directory, 'assets', 'images', 'ships', 'cruiser', 'cruiser.png'), (200, 600), (40, 195),
-                2, os.path.join(current_directory, 'assets', 'images', 'ships', 'cruiser', 'cruisergun.png'), (0.4, 0.125), [-0.36, 0.64]],
-    'destroyer': ['destroyer', os.path.join(current_directory, 'assets', 'images', 'ships', 'destroyer', 'destroyer.png'), (275, 600), (30, 145),
-                  2, os.path.join(current_directory, 'assets', 'images', 'ships', 'destroyer', 'destroyergun.png'), (0.5, 0.15), [-0.52, 0.71]],
-    'patrol boat': ['patrol boat', os.path.join(current_directory, 'assets', 'images', 'ships', 'patrol boat', 'patrol boat.png'), (425, 600), (20, 95),
+    current_directory = os.path.dirname(__file__)
+    FLEET = {
+        'battleship': ['battleship', os.path.join(current_directory, 'assets', 'images', 'ships', 'battleship', 'battleship.png'), (125, 600), (40, 195),
+                    4, os.path.join(current_directory, 'assets', 'images', 'ships', 'battleship', 'battleshipgun.png'), (0.4, 0.125), [-0.525, -0.34, 0.67, 0.49]],
+        'cruiser': ['cruiser', os.path.join(current_directory, 'assets', 'images', 'ships', 'cruiser', 'cruiser.png'), (200, 600), (40, 195),
+                    2, os.path.join(current_directory, 'assets', 'images', 'ships', 'cruiser', 'cruisergun.png'), (0.4, 0.125), [-0.36, 0.64]],
+        'destroyer': ['destroyer', os.path.join(current_directory, 'assets', 'images', 'ships', 'destroyer', 'destroyer.png'), (275, 600), (30, 145),
+                    2, os.path.join(current_directory, 'assets', 'images', 'ships', 'destroyer', 'destroyergun.png'), (0.5, 0.15), [-0.52, 0.71]],
+        'patrol boat': ['patrol boat', os.path.join(current_directory, 'assets', 'images', 'ships', 'patrol boat', 'patrol boat.png'), (425, 600), (20, 95),
                     0, '', None, None],
-    'submarine': ['submarine', os.path.join(current_directory, 'assets', 'images', 'ships', 'submarine', 'submarine.png'), (350, 600), (30, 145),
-                  1, os.path.join(current_directory, 'assets', 'images', 'ships', 'submarine', 'submarinegun.png'), (0.25, 0.125), [-0.45]],
-    'carrier': ['carrier', os.path.join(current_directory, 'assets', 'images', 'ships', 'carrier', 'carrier.png'), (50, 600), (45, 245),
-                0, '', None, None],
-    'rescue ship': ['rescue ship', os.path.join(current_directory, 'assets', 'images', 'ships', 'rescue ship', 'rescue ship.png'), (500, 600), (20, 95),
+        'submarine': ['submarine', os.path.join(current_directory, 'assets', 'images', 'ships', 'submarine', 'submarine.png'), (350, 600), (30, 145),
+                    1, os.path.join(current_directory, 'assets', 'images', 'ships', 'submarine', 'submarinegun.png'), (0.25, 0.125), [-0.45]],
+        'carrier': ['carrier', os.path.join(current_directory, 'assets', 'images', 'ships', 'carrier', 'carrier.png'), (50, 600), (45, 245),
+                    0, '', None, None],
+        'rescue ship': ['rescue ship', os.path.join(current_directory, 'assets', 'images', 'ships', 'rescue ship', 'rescue ship.png'), (500, 600), (20, 95),
                     0, '', None, None]
-}
-STAGE = ['Main Menu', 'Deployment', 'Game Over']
+    }
+    STAGE = ['Main Menu', 'Deployment', 'Game Over']
 
-MAINMENUIMAGE = loadImage(os.path.join(current_directory, 'assets', 'images', 'background', 'Battleship.jpg'), (SCREENWIDTH // 3 * 2, SCREENHEIGHT))
-ENDSCREENIMAGE = loadImage(os.path.join(current_directory, 'assets', 'images', 'background', 'Carrier.jpg'), (SCREENWIDTH, SCREENHEIGHT))
-BUTTONIMAGE = loadImage(os.path.join(current_directory, 'assets', 'images', 'buttons', 'button.png'), (150, 50))
-BUTTONIMAGE1 = loadImage(os.path.join(current_directory, 'assets', 'images', 'buttons', 'button.png'), (250, 100))
-BUTTONS = [
-    Button(BUTTONIMAGE, (150, 50), (25, 900), 'Randomize'),
-    Button(BUTTONIMAGE, (150, 50), (200, 900), 'Reset'),
-    Button(BUTTONIMAGE, (150, 50), (375, 900), 'Deploy')
-]
-difficulty_buttons = [
-    Button(BUTTONIMAGE1, (250, 100), (900, SCREENHEIGHT // 2 - 150), 'Easy Bot'),
-    Button(BUTTONIMAGE1, (250, 100), (900, SCREENHEIGHT // 2 + 150), 'Hard Bot')
-]
-REDTOKEN = loadImage(os.path.join(current_directory, 'assets', 'images', 'tokens', 'redtoken.png'), (CELLSIZE, CELLSIZE))
-GREENTOKEN = loadImage(os.path.join(current_directory, 'assets', 'images', 'tokens', 'greentoken.png'), (CELLSIZE, CELLSIZE))
-BLUETOKEN = loadImage(os.path.join(current_directory, 'assets', 'images', 'tokens', 'bluetoken.png'), (CELLSIZE, CELLSIZE))
-TOKENS = []
+    MAINMENUIMAGE = loadImage(os.path.join(current_directory, 'assets', 'images', 'background', 'Battleship.jpg'), (SCREENWIDTH // 3 * 2, SCREENHEIGHT))
+    ENDSCREENIMAGE = loadImage(os.path.join(current_directory, 'assets', 'images', 'background', 'Carrier.jpg'), (SCREENWIDTH, SCREENHEIGHT))
+    BUTTONIMAGE = loadImage(os.path.join(current_directory, 'assets', 'images', 'buttons', 'button.png'), (150, 50))
+    BUTTONIMAGE1 = loadImage(os.path.join(current_directory, 'assets', 'images', 'buttons', 'button.png'), (250, 100))
+    BUTTONS = [
+        Button(BUTTONIMAGE, (150, 50), (25, 900), 'Randomize'),
+        Button(BUTTONIMAGE, (150, 50), (200, 900), 'Reset'),
+        Button(BUTTONIMAGE, (150, 50), (375, 900), 'Deploy')
+    ]
+    difficulty_buttons = [
+        Button(BUTTONIMAGE1, (250, 100), (900, SCREENHEIGHT // 2 - 150), 'Easy Bot'),
+        Button(BUTTONIMAGE1, (250, 100), (900, SCREENHEIGHT // 2 + 150), 'Hard Bot')
+    ]
+    REDTOKEN = loadImage(os.path.join(current_directory, 'assets', 'images', 'tokens', 'redtoken.png'), (CELLSIZE, CELLSIZE))
+    GREENTOKEN = loadImage(os.path.join(current_directory, 'assets', 'images', 'tokens', 'greentoken.png'), (CELLSIZE, CELLSIZE))
+    BLUETOKEN = loadImage(os.path.join(current_directory, 'assets', 'images', 'tokens', 'bluetoken.png'), (CELLSIZE, CELLSIZE))
+    TOKENS = []
 
+    #creating game variables
+    pGameGrid = createGameGrid(ROWS, COLS, CELLSIZE, (50, 50))
+    pGameLogic = createGameLogic(ROWS, COLS)
+    pFleet = createFleet()
 
-#creating game variables
-pGameGrid = createGameGrid(ROWS, COLS, CELLSIZE, (50, 50))
-pGameLogic = createGameLogic(ROWS, COLS)
-pFleet = createFleet()
+    cGameGrid = createGameGrid(ROWS, COLS, CELLSIZE, (SCREENWIDTH - (ROWS * CELLSIZE) - 50, 50))
+    cGameLogic = createGameLogic(ROWS, COLS)
+    cFleet = createFleet()
+    randomizeShipPositions(cFleet, cGameGrid)
 
-cGameGrid = createGameGrid(ROWS, COLS, CELLSIZE, (SCREENWIDTH - (ROWS * CELLSIZE) - 50, 50))
-cGameLogic = createGameLogic(ROWS, COLS)
-cFleet = createFleet()
-randomizeShipPositions(cFleet, cGameGrid)
+    printGameLogic()
 
-printGameLogic()
+    player1 = Player(globals())
+    computer = EasyComputer(globals())
 
-player1 = Player(globals())
-computer = EasyComputer(globals())
+    winner = None
 
-winner = None
+    #game flow
+    RUNGAME = True
+    while RUNGAME:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUNGAME = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if DEPLOYMENT == True:
+                        for ship in pFleet:
+                            if ship.rect.collidepoint(pygame.mouse.get_pos()):
+                                ship.active = True
+                                sortFleet(ship, pFleet)
+                                ship.selectShipAndMove(pFleet)
 
+                    else:
+                        if player1.turn == True:
+                            player1.makeAttack(cGameGrid, cGameLogic)
+                            if player1.turn == False:
+                                TURNTIMER = pygame.time.get_ticks()
 
-#game flow
-RUNGAME = True
-while RUNGAME:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUNGAME = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if DEPLOYMENT == True:
-                    for ship in pFleet:
-                        if ship.rect.collidepoint(pygame.mouse.get_pos()):
-                            ship.active = True
-                            sortFleet(ship, pFleet)
-                            ship.selectShipAndMove(pFleet)
+                    if GAMESTATE == STAGE[0] or GAMESTATE == STAGE[2]:
+                        for button in difficulty_buttons:
+                            if button.rect.collidepoint(pygame.mouse.get_pos()):
+                                if button.name == 'Easy Bot':
+                                    computer = EasyComputer(globals())
+                                elif button.name == 'Hard Bot':
+                                    computer = HardComputer(globals())
+                                GAMESTATE = STAGE[1]
+                    elif GAMESTATE == STAGE[1]:
+                        for button in BUTTONS:
+                            if button.rect.collidepoint(pygame.mouse.get_pos()):
+                                if button.name == 'Deploy':
+                                    DEPLOYMENT = deploymentPhase(DEPLOYMENT)
+                                elif button.name == 'Redeploy':
+                                    restartGame(DEPLOYMENT)
+                                elif button.name == 'Quit':
+                                    RUNGAME = False
+                                elif button.name == 'Randomize':
+                                    randomizeShipPositions(pFleet, pGameGrid)
+                                    randomizeShipPositions(cFleet, cGameGrid)
+                                elif button.name == 'Reset':
+                                    resetShips(pFleet)
+                
+                elif event.button == 2:
+                    printGameLogic()
+        
+        updateGameScreen(GAMESCREEN)
 
-                else:
-                    if player1.turn == True:
-                        player1.makeAttack(cGameGrid, cGameLogic)
-                        if player1.turn == False:
-                            TURNTIMER = pygame.time.get_ticks()
+        if GAMESTATE == STAGE[1] and DEPLOYMENT != True:
+            player1Wins = checkForWinners(cGameLogic)
+            computerWins = checkForWinners(pGameLogic)
+            if player1Wins == True or computerWins == True:
+                GAMESTATE = STAGE[2]
+                if player1Wins == True:
+                    winner = 'Player won!'
+                elif computerWins == True:
+                    winner = 'Computer won!'
 
-                if GAMESTATE == STAGE[0] or GAMESTATE == STAGE[2]:
-                    for button in difficulty_buttons:
-                        if button.rect.collidepoint(pygame.mouse.get_pos()):
-                            if button.name == 'Easy Bot':
-                                computer = EasyComputer(globals())
-                            elif button.name == 'Hard Bot':
-                                computer = HardComputer(globals())
-                            GAMESTATE = STAGE[1]
-                elif GAMESTATE == STAGE[1]:
-                    for button in BUTTONS:
-                        if button.rect.collidepoint(pygame.mouse.get_pos()):
-                            if button.name == 'Deploy':
-                                DEPLOYMENT = deploymentPhase(DEPLOYMENT)
-                            elif button.name == 'Redeploy':
-                                restartGame(DEPLOYMENT)
-                            elif button.name == 'Quit':
-                                RUNGAME = False
-                            elif button.name == 'Randomize':
-                                randomizeShipPositions(pFleet, pGameGrid)
-                                randomizeShipPositions(cFleet, cGameGrid)
-                            elif button.name == 'Reset':
-                                resetShips(pFleet)
-            
-            elif event.button == 2:
-                printGameLogic()
-    
-    updateGameScreen(GAMESCREEN)
+        takeTurns(player1, computer)
 
-    if GAMESTATE == STAGE[1] and DEPLOYMENT != True:
-        player1Wins = checkForWinners(cGameLogic)
-        computerWins = checkForWinners(pGameLogic)
-        if player1Wins == True or computerWins == True:
-            GAMESTATE = STAGE[2]
-            if player1Wins == True:
-                winner = 'Player won!'
-            elif computerWins == True:
-                winner = 'Computer won!'
+    pygame.quit()
 
-    takeTurns(player1, computer)
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
